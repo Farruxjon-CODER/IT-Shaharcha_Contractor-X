0aa73100f04a6d0aa0049eb7e7483547ae4e3320
Ibrohim <aliv52254@email.com>
2026-01-02 15:54:39 +0500

    Blog

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { parseStyle } from 'ant-design-vue/es/_util/cssinjs/hooks/useStyleRegister';
 import { ref } from 'vue';
 
 const title = ref('Contact Component');
@@ -8,6 +9,45 @@ const title = ref('Contact Component');
     <div>
         <h1>{{ title }}</h1>
     </div>
+    <h5>/BLOG</h5>
+    <h2>Articles & News</h2>
+    <button class="button">Browse articles</button>
+    <div class="img1">
+    <img src="" alt="" class="img1">
+    <h2>12 designers tricks for picking the perfect home color palette</h2>
+    <p>June 17, 2022</p>
+    </div>
+    <div class="img2">
+    <img src="" alt="">
+    <h2>Paint trends 2022 - the 17 colors you need for the ultimate wonder walls</h2>
+    <p>June 17, 2022</p>
+    </div>
+    <div class="img3">
+    <img src="" alt="">
+    <h2>Clever DIY home improvements you can do during the pandemic</h2>
+    <p>June 17, 2022</p>
+    </div>
+    <div class="img4">
+    <img src="" alt="">
+    <h2>15 mind blowing floor designs to make your home look great</h2>
+    <p>June 17, 2022</p>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.button {
+    float: right;
+};
+.img1{
+    float: left;
+}
+.img2{
+    float: right;
+}
+.img3{
+    float: right;
+}
+.img4{
+    float: right;
+}
+</style>
