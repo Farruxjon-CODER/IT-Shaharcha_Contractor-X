4ae6e73b5fc45c90f073687a21b61b15253ffd73
Madina <bonumadina864@email.com>
2026-01-02 16:01:38 +0500

    Home

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -7,6 +7,11 @@ const title = ref('Pages Component');
 <template>
     <div>
         <h1>{{ title }}</h1>
+        <h1>Ready to pull the trigger? Get a quote today.</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit dolor sit varius rhoncus, nullam pulvinar ut sollicitudin dictumst porttitor porttitor quis.</p>
+        <button>Get a quote</button>
+        <button>Our services</button>
+        <img src="">
     </div>
 </template>
 
